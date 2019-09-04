1371ecbc3a7cd70d7c339fcb49341bc451abed3c
Benjamin Duroule <beduroul@z3r2p3.le-101.fr>
2019-09-04 17:49:36 +0200

    window

--- include/wolf.h
+++ include/wolf.h
@@ -4,4 +4,26 @@
 # include "../libft/include/libft.h"
 # include "../minilibx_macos/mlx.h"
 
+#define WIDTH 1280
+#define HEIGHT 720
+
+typedef struct			s_env
+{
+	void				*mlx_ptr;
+	void				*win_ptr;
+	void				*ptr_img;
+	char				*my_str_img;
+    int					bpp;
+	int					s_l;
+    int					endian;
+    double              posx;
+    double              posy;
+    double              dirx;
+    double              diry;
+    double              px;
+    double              py;
+    double              time;
+    double              otim;
+}						t_env;
+
 #endif
--- srcs/main.c
+++ srcs/main.c
@@ -1,7 +1,16 @@
 #include "wolf.h"
 
-int     main(int ac, char **av)
+int     main(void)
 {
-    
+    t_env *env;
+
+    if (!(env = (t_env *)malloc(sizeof(t_env))))
+        return (0);
+	env->mlx_ptr = mlx_init();
+	env->win_ptr = mlx_new_window(env->mlx_ptr, WIDTH, HEIGHT, "Wolf3d");
+	env->ptr_img = mlx_new_image(env->mlx_ptr, WIDTH, HEIGHT);
+	env->my_str_img = mlx_get_data_addr(env->ptr_img, &(env->bpp), &(env->s_l), &(env->endian));
+    mlx_put_image_to_window(env->mlx_ptr, env->win_ptr, env->ptr_img, 0, 0);
+    	mlx_loop(env->mlx_ptr);
     return (0);
 }
